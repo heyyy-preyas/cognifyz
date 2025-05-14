40b18b140b0298f359e8d6ae8bc576326b1fe4c9
Preyas Pandya <preyaspandya8818@gmail.com>
2025-05-15 00:23:33 +0530

    Add files via upload

---
+++ LEVEL 3 TASK 2 (Final Page)/finalwebpage.js
@@ -0,0 +1,7 @@
+function showContactForm() {
+    alert("To apply, please send your resume and cover letter to internships@cognifyztech.com.");
+}
+
+function learnMore() {
+    alert("For more information, visit our website at www.cognifyztech.com or contact us directly.");
+}
